378f6303f2352a50f1daba1530fb40941f9f4a49
carterisonline <me@carteris.online>
2022-09-10 23:04:45 -0400

    Removed Vercel SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,8 @@ import { defineConfig } from 'astro/config';
 import solid from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://carteris.online',
-  integrations: [tailwind(), solid()],
-  output: "server",
-  adapter: vercel()
+	site: 'https://carteris.online',
+	integrations: [tailwind(), solid()],
 });
