789b3d90cdcf7af7e058c5b04245ea53744e81fb
carterisonline <me@carteris.online>
2022-09-10 22:42:08 -0400

    Added SSR support with Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from 'astro/config';
 import solid from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://carteris.online',
-	integrations: [tailwind(), solid()],
+  site: 'https://carteris.online',
+  integrations: [tailwind(), solid()],
+  output: "server",
+  adapter: vercel()
 });
