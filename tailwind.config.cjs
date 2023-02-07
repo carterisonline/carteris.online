55f25768f401a7905696dbbcfa608d1bfded7d0f
carterisonline <me@carteris.online>
2023-02-06 22:24:31 -0500

    Added the projects page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,5 +6,26 @@ module.exports = {
 	theme: {
 		extend: {},
 	},
-	plugins: [plugin(({ matchUtilities, theme }) => {})],
+	theme: {
+		extend: {
+			textShadow: {
+				sm: '0 1px 2px var(--tw-shadow-color)',
+				md: '0 2px 4px var(--tw-shadow-color)',
+				lg: '0 4px 12px var(--tw-shadow-color)',
+				xl: '0 16px 32px var(--tw-shadow-color)',
+			},
+		},
+	},
+	plugins: [
+		plugin(function ({ matchUtilities, theme }) {
+			matchUtilities(
+				{
+					'text-shadow': value => ({
+						textShadow: value,
+					}),
+				},
+				{ values: theme('textShadow') }
+			);
+		}),
+	],
 };
