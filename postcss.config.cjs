3030fc7cc873fc3f349693833a44a5ea4d93f6a0
Carter Reeb <me@carteris.online>
2024-11-19 16:44:20 -0500

    Switch to a boring, but arguably way more useful static website

--- postcss.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	plugins: [
-		require('postcss-import'),
-		require('tailwindcss/nesting'),
-		reqiore('tailwindcss'),
-		require('autoprefixer'),
-		require('cssnano'),
-	],
-};
