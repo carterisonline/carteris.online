b1ac7bdb966e8fbe46c3ad151efd59c821140c78
Carter Reeb <me@carteris.online>
2025-05-21 19:14:02 -0400

    Switch to event listeners for cloudflare

--- preload.js
+++ preload.js
@@ -1,8 +1,8 @@
-window.onload = function () {
+document.addEventListener("DOMContentLoaded", function () {
     const parser = new DOMParser();
     fetch("/components/header.html").then(file => file.text().then(content => {
         const header = parser.parseFromString(content, "text/html").body.querySelector("header");
         document.body.style.paddingTop = "1em";
         document.body.prepend(header);
     }));
-}
+});
